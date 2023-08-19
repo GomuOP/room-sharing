03c51915f73eb7a9f90b494d808f16f945ca464e
Gomtesh <gomteshrawat2002@gmail.com>
2023-08-19 19:10:51 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,76 @@ import logo from './logo.svg';
 import './App.css';
 import { Auth } from "./components/auth"
 import { db, auth } from "./config/firebase"
+import { useEffect, useState } from 'react';
+import { getDocs, collection, addDoc, deleteDoc, doc } from "firebase/firestore";
 
 function App() {
+  const [personList, setPersonList]=useState([]);
+
+  const [personName, setPersonName]=useState("");
+  const [personLocation, setpersonLocation]=useState("");
+  const [personContact, setPersonContact]=useState(0);
+
+  const personCollectionRef = collection(db,"persons");
+  const getlist = async () =>{
+    try{
+      const data=await getDocs(personCollectionRef);
+      console.log(data);
+      const filteredData=data.docs.map((doc)=>({
+        ...doc.data(),
+        id:doc.id,
+      }))
+      setPersonList(filteredData);
+    }catch(err){
+      console.error(err);
+    }
+  };
+  useEffect(()=>{
+    
+    getlist();
+  },[]);
+
+  const onSubmitList = async () =>{
+    await addDoc(personCollectionRef,{
+      
+      contact: personContact,
+      location: personLocation,
+      name: personName,
+      userId: auth?.currentUser?.uid
+      
+    })
+    getlist();
+  }
+  const deleteList = async (id)=>{
+    const listDoc=doc(db,"persons",id);
+    await deleteDoc(listDoc);
+    getlist();
+  }
   return (
     <div className="App">
       <Auth/>
+      <div>
+        <input placeholder="Name..."
+          onChange={(e)=>setPersonName(e.target.value)}
+        />
+        <input placeholder="Location..."
+          onChange={(e)=>setpersonLocation(e.target.value)}
+        />
+        <input placeholder="Contact..." type="number"
+          onChange={(e)=>setPersonContact(e.target.value)}
+        />
+        <button onClick={onSubmitList}>Submit</button>
+      </div>
+      <div>
+        {personList.map((p)=>(
+          <div>
+            <h1>{p.name}</h1>
+            <p>{p.location}</p>
+            <p>{p.contact}</p>
+            <button onClick={()=>deleteList(p.id)}>Delete </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBuGTat1Fgy70ZPldXylXZEbJUo1084rHw",
-  authDomain: "todo-3a683.firebaseapp.com",
-  projectId: "todo-3a683",
-  storageBucket: "todo-3a683.appspot.com",
-  messagingSenderId: "8968899086",
-  appId: "1:8968899086:web:9a90710bc1221c251b0a5f",
-  measurementId: "G-JW1SK7T2Z2"
+    apiKey: "AIzaSyB2euHKmoL3Zdkjo7TdzYAVxCPO0WJrWA4",
+    authDomain: "r-sharing.firebaseapp.com",
+    projectId: "r-sharing",
+    storageBucket: "r-sharing.appspot.com",
+    messagingSenderId: "1036665690399",
+    appId: "1:1036665690399:web:f2558e407f88e35f9a382a",
+    measurementId: "G-B48FBW41V8"
 };
 
 const app = initializeApp(firebaseConfig);
